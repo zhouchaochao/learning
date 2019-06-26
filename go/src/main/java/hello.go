c21b0dc3ddbdaa9959dcd6ec499b94fcb519c181
zhouzhichao <zhouzhichao@didichuxing.com>
2019-06-26 14:28:30 +0800

    go

---
+++ go/src/main/java/hello.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, World!")
+
+    rrr := "Runoob" // var f string = "Runoob"
+    fmt.Println(rrr)
+
+    g, h := 123, "hello"
+    fmt.Println(g, h)
+
+    var c, d int = 1, 2
+    fmt.Println(c, d)
+
+    var e, f = 123, "hello"
+    fmt.Println(e, f)
+}
