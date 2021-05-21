9c888d40c86174995a1f45550aaf965e3a68fea5
qiao.zzc <qiao.zzc@autonavi.com>
2021-05-21 15:45:55 +0800

    多线程ExecutorCompletionService

--- common-test/src/main/java/com/cc/multiThread/CompletionServiceTest.java
+++ common-test/src/main/java/com/cc/multiThread/CompletionServiceTest.java
@@ -1,8 +1,13 @@
 package com.cc.multiThread;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Random;
 import java.util.concurrent.*;
 
@@ -22,7 +27,8 @@ public class CompletionServiceTest {
 
     public static void main(String[] args) {
         try {
-            new CompletionServiceTest().completionServiceTest();
+            //new CompletionServiceTest().completionServiceTest();
+            new CompletionServiceTest().concurrentTest();
         } catch (Exception e) {
             LOGGER.error("errorMessage={}", e.getMessage(), e);
         }
@@ -69,6 +75,65 @@ public class CompletionServiceTest {
         } catch (Exception e) {
             LOGGER.error("errorMessage={}", e.getMessage(), e);
         }
+    }
+
+    public void concurrentTest() {
+
+        ExecutorService COMMON_EXECUTOR_SERVICE = new ThreadPoolExecutor(10, 10,
+            60L, TimeUnit.SECONDS,
+            new LinkedBlockingQueue<>(1000000), new ThreadFactoryBuilder().setNameFormat("common-pool-%d").build(), new ThreadPoolExecutor.DiscardPolicy());
+
+        try {
+            List<String> configServerIps = new ArrayList<>();
+            configServerIps.add("a");
+            configServerIps.add("b");
+
+            Map<String, String> transferMap = new ConcurrentHashMap<>(configServerIps.size());
+            CompletionService<String> completionServiceBase = new ExecutorCompletionService<>(COMMON_EXECUTOR_SERVICE);
+            for (String configServerIp : configServerIps) {
+                completionServiceBase.submit(new Callable<String>() {
+                    @Override
+                    public String call() {
+                        String value = getValue(configServerIp);
+                        if (true) {
+                            //throw new RuntimeException("businessException configserver:" + configServerIp);
+                        }
+
+                        transferMap.put(configServerIp, value);
+                        return value;
+                    }
+                });
+            }
+
+            for (int i = 0; i < configServerIps.size(); i++) {
+
+                //Future<String> future = completionServiceBase.poll(2, TimeUnit.MILLISECONDS);
+                Future<String> future = completionServiceBase.poll(100, TimeUnit.MILLISECONDS);
+                if (future != null) {
+                    try {
+                        //如果有业务异常，get的时候抛出。如果不get,业务异常是不会抛出的
+                        future.get();
+                    } catch (Exception e) {
+                        LOGGER.error("get business execption from call.errorMessage={}",e.getMessage(), e);
+                        throw e;
+                    }
+                } else {
+                    LOGGER.warn("业务执行超时");
+                    throw new TimeoutException("业务执行超时");
+                }
+            }
+
+            for (Entry<String, String> entry : transferMap.entrySet()) {
+                LOGGER.info("{} -> {}", entry.getKey(), entry.getValue());
+            }
+
+        } catch (Exception e) {
+            LOGGER.error("errorMessage={}", e.getMessage(), e);
+        }
+    }
+
+    private String getValue(String ip) {
+        return ip + ip;
     }
 
 }
