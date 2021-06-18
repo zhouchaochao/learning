7e2ed157f145dcb4b109f76d9ab85e0234c40f42
qiao.zzc <qiao.zzc@autonavi.com>
2021-06-18 16:12:21 +0800

    CompletableFutureTest

---
+++ common-test/src/main/java/com/cc/multiThread/CompletableFutureTest.java
@@ -0,0 +1,90 @@
+package com.cc.multiThread;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Description: CompletableFutureTest
+ * Date:  2021/6/18
+ *
+ * @author <a href=mailto:qiao.zzc@autonavi.com>yuhu</a>
+ */
+
+public class CompletableFutureTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(CompletableFutureTest.class);
+
+    private Executor executor = new ThreadPoolExecutor(240, 10000, 500, TimeUnit.SECONDS,
+        new SynchronousQueue<Runnable>(), new ThreadFactoryBuilder().setNameFormat("thread-pool-%d").build());
+
+    public static void main(String[] args) {
+        new CompletableFutureTest().test();
+        System.exit(0);
+    }
+
+    public void test() {
+        try {
+
+            Map<Integer, CompletableFuture<String>> completableFutureMap = new HashMap<>();
+
+            for (int i = 0; i < 10; i++) {
+                final Integer param = i;
+                CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() -> {
+
+                    if (param == 2) {
+                        try {
+                            Thread.sleep(1001);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    if (param == 3) {
+                        throw new IllegalArgumentException("IllegalArgumentException");
+                    }
+
+                    if (param == 4) {
+                        throw new RuntimeException("RuntimeException");
+                    }
+
+                    if (param == 5) {
+                        return null;
+                    }
+
+                    String result = "" + param;
+                    return result;
+                }, executor);
+                completableFutureMap.put(i, completableFuture);
+            }
+
+            for (Entry<Integer, CompletableFuture<String>> integerCompletableFutureEntry : completableFutureMap
+                .entrySet()) {
+                try {
+                    String value = integerCompletableFutureEntry.getValue().get(1000, TimeUnit.MILLISECONDS);
+                    System.out.println(integerCompletableFutureEntry.getKey() + " " + value);
+                } catch (TimeoutException e) {
+                    System.out.println(integerCompletableFutureEntry.getKey() + " " + "执行超时 throw TimeoutException");
+                } catch (ExecutionException e) {
+                    System.out.println(integerCompletableFutureEntry.getKey() + " " + "执行抛异常 throw ExecutionException");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+        } catch (Exception e) {
+            logger.error("errorMessage={}", e.getMessage(), e);
+        }
+    }
+}
