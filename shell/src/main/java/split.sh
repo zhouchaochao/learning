e587a710f37bac69f127f4e23ee80228f28483cf
zhouchaochao <youdonotknow110>
2023-04-19 20:59:17 +0800

    split

---
+++ shell/src/main/java/split.sh
@@ -0,0 +1,10 @@
+切分合并文档
+
+示例：
+split -b 450M before.dump.202204121048.dump.tar.gz before.dump.202204121048.dump.tar.gz.split
+切分，按照每个文件450M的大小进行切分
+
+cat before.dump.202204121048.dump.tar.gz.splita* > before.dump.202204121048.dump.tar.gz
+合并，将多个文件splita*合并为一个
+
+
